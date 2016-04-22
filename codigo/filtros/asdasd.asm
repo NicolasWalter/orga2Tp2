a199051ca1afcd7cf688998dce135a8f04a46640
sticco patricio bernardo <pbsticco@ws19.labo5>
2016-04-21 21:53:26 -0300

    no anda cropflip

--- codigo/filtros/asdasd.asm
+++ codigo/filtros/asdasd.asm
@@ -9,55 +9,75 @@ section .text
 ;void cropflip_asm(unsigned char *src,  unsigned char *dst, int cols, int filas, int src_row_size, int dst_row_size, int tamx, int tamy, int offsetx, int offsety);
 ; 						RDI,      					RSI,     RDX ,     RCX,        R8,             R9
 
-cropflip_asm:
+ cropflip_asm:
 push rbp
 mov rbp, rsp
 push r12
 push r13
+push r14
+push r15
 
 ; imul rcx, rdx
 ; shr rcx, 2  
 xor xmm0,xmm0
 pxor xmm7, xmm7
-xor r13,r13
-xor r12,r12 
-xor r11, r11
-xor r10, r10
-;xor rdx,rdx
+xor rcx, rcx
+xor r8, r8
+xor r9, r9
 xor rax, rax
-mov r11, [rbp+16]  ;tam x
-mov r12, [rbp+24]  ; tamy
-mov r13, [rbp+32]  ; offsetx
+xor r10, r10 ; indice "i" fuente
+xor r11, r11  ; indice "j" fuente
+xor r12,r12 ; indice i destino
+xor r13,r13 ; indice j destino
+xor r14, r14
+xor r15, r15
+mov r9, [rbp+16]  ;tam x CAPAZ PISO CACA 
+mov r14, [rbp+24]  ; tamy
+mov r15, [rbp+32]  ; offsetx
 mov rax, [rbp+40]  ;offsety
-mov r10, rax ; EN R10 TENGO EL INDICE i, arranca en offsety
-;mov r11, rdx ; en r11 indice J, arranca en offsetx 
-
-
-mov r8,r11
-shr r8,2 ; tamx/4
-mov r9,0 ;mi i
+mov r8, r14 ; TIENE TAMY
 ; “puntero al inicio de la matriz” + “cantidad elementos de la fila” * “indice de fila” * “tamaño dato” ++ “indice de columna” * “tamano dato”
-.cicloExterior:
-	cmp r9,r12 ; i<tamy
-	jne .preCicloInterior
+.ciclo:
+
+	cmp r10, r8  ; si el indice de la fila != tamy
+	jne .seguir
+
 	jmp .fin
-	.preCicloInterior
-	mov r10,0
-	.cicloInterior:
-		cmp r10,r8 ; j con tamx/4
-		jne .seguirInterior
-		jmp .finCicloInterior
-		.seguirInterior:
-		movups xmm0,[rdi] ;meti 4 pixeles
 
-		movups xmm1,[rdi+]
-
-		lea rdi, [rdi + tam_4pxs]; muevo el puntero al siguiente bloque de 4pxs
-		inc r10
-		jmp .cicloInterior
+	.seguir:
+	add r14,rax
+	sub r14,r10
+	dec r14
+	imul r14, rdx
+	movdqu xmm0, [rdi+r14+(r15+r11)]  ; si copie bien, formula sacada de la diapo para moverme en matrices
+	mov r14, r8
+	;tamy + of f sety − i − 1, of f setx + j
+	
 
 
-	.finCicloInterior:
-	lea rdi, [rdi + rdx - r11];super salto
-	inc r9
-	jmp .cicloExterior
+	cmp r11, rdx
+	je .cambioFila
+	inc r11
+	jmp .ciclo
+		.cambioFila:
+			inc r10
+			mov r11, r13
+			jmp .ciclo
+
+
+
+
+
+
+
+
+
+
+
+; .fin:
+; pop r15
+; pop r14
+; pop r13
+; pop r12
+; pop rbp
+ ret
--- codigo/filtros/cropflip_asm.asm
+++ codigo/filtros/cropflip_asm.asm
@@ -1,5 +1,7 @@
- %define TAM_4PXS 4
-; %define TAMX [rbp+16]
+ %define tam_4pxs 16
+section .data
+ multiplicadores: DB 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
+ ; %define TAMX [rbp+16]
 ; %define TAMY [rbp+24]
 ; %define OFFSETX [rbp+32]
 ; %define OFFSETY [rbp+40]
@@ -19,7 +21,7 @@ push r15
 
 ; imul rcx, rdx
 ; shr rcx, 2  
-pxor xmm0,xmm0
+pxor xmm0, xmm0
 pxor xmm7, xmm7
 xor rcx, rcx
 xor r8, r8
@@ -27,45 +29,56 @@ xor r9, r9
 xor rax, rax
 xor r10, r10 ; indice "i" fuente
 xor r11, r11  ; indice "j" fuente
-xor r12,r12 ; indice i destino
-xor r13,r13 ; indice j destino
+; xor r12,r12 ; indice i destino
+; xor r13,r13 ; indice j destino
 xor r14, r14
 xor r15, r15
-mov r9, [rbp+16]  ;tam x CAPAZ PISO CACA 
-mov r14, [rbp+24]  ; tamy
-mov r15, [rbp+32]  ; offsetx
-mov rax, [rbp+40]  ;offsety
-mov r8, r14 ; TIENE TAMY
+mov r9d, [rbp+16]  ;tam x CAPAZ PISO CACA 
+mov r14d, [rbp+24]  ; tamy
+mov r15d, [rbp+32]  ; offsetx
+mov eax, [rbp+40]  ;offsety
+mov r8d, r14d ; TIENE TAMY
 ; “puntero al inicio de la matriz” + “cantidad elementos de la fila” * “indice de fila” * “tamaño dato” ++ “indice de columna” * “tamano dato”
 .ciclo:
 
-	cmp r10, r8  ; si el indice de la fila != tamy
-	jne .seguir
+	cmp r10, r14  ; si el indice de la fila = tamy
+	je .fin
+	.cicloJ:
+	
+	cmp r11, r9 ;veo si r11 llego al tope de la fila
+	jge .cambioFila
 
-	jmp .fin
+	add r14, rax 	;tamy + off
+	sub r14, r10	;tamy + off - i
+	dec r14			;tamy + off -i -1
+	mov r12, rax
+	mov rax, rdx
+	mul r14			;(tamy + off -i -1)*cols
+	mov r14, rax
+	mov rax, r12
 
-	.seguir:
+	add r15, r11	;ox+j
 
-		cmp r11, r9
-	je .cambioFila
-	inc r11
-	jmp .ciclo
-		.cambioFila:
-			inc r10
-			mov r11, 0
-			jmp .ciclo
-	add r14,rax ;tamy + off
-	sub r14,r10;tamy + off - i
-	dec r14;tamy + off -i -1
-	imul r14, rdx;(tamy + off -i -1)*cols
-	add r15, r11;ox+j
 	add r14, r15;
-	movdqu xmm0, [rdi+r14*4]  ; si copie bien, formula sacada de la diapo para moverme en matrices
+	movdqu xmm0, [rdi+r14 * 4]  ; si copie bien, formula sacada de la diapo para moverme en matrices
+	
 	mov r14, r8
 	sub r15, r11
 
+	
+
 	movdqu [rsi], xmm0
-	add rsi, 16
+	lea rsi, [rsi+tam_4pxs]
+
+
+	;inc r11
+	add r11, 4
+	jmp .cicloJ
+
+	.cambioFila
+	inc r10
+	mov r11, 0
+	jmp .ciclo
 
 	;tamy + of f sety − i − 1, of f setx + j
 	
@@ -79,4 +92,10 @@ pop r14
 pop r13
 pop r12
 pop rbp
- ret
+ret
+
+	;Prueba para poner todo en blanco
+	; pcmpeqw xmm7,xmm7
+	; movups [rsi], xmm7
+	; lea rsi, [rsi+tam_4pxs]
+	;posta
