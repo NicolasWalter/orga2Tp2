58134ccabc5cbfe8b1dd0761ba9a18d45bd33d8f
sticco patricio bernardo <pbsticco@ws4.labo1>
2016-04-25 20:35:41 -0300

    habia dejado mierda en ldr de C

--- codigo/filtros/ldr_c.c
+++ codigo/filtros/ldr_c.c
@@ -19,15 +19,15 @@ void ldr_c    (
     unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 
     int i=0;
-    for (i; i < 16; i++)
+    for (i; i < filas; i++)
     {
         int j = 0;
-        for (j; j < 12; j++)
+        for (j; j < cols; j++)
         {
             bgra_t *p_d = (bgra_t*) &dst_matrix[i][j * 4];
             bgra_t *p_s = (bgra_t*) &src_matrix[i][j * 4];
             *p_d = *p_s;
-           /* if(!(i<2 || j<2 || i+2 >= filas || j+2 >= cols) ){
+            if(!(i<2 || j<2 || i+2 >= filas || j+2 >= cols) ){
 
                 double maxi =5 * 5 * 255 * 3 * 255;
                 double sumaRij=0;
@@ -42,8 +42,7 @@ void ldr_c    (
                        sumaBij=sumaBij+((bgra_t*) &dst_matrix[i+iAux][(j+jAux) * 4])->b;
                    }
                 }
-                printf("%d %d %d %d %d\n",i ,j, ((bgra_t*) &src_matrix[i][(j) * 4])->r,((bgra_t*) &src_matrix[i][(j) * 4])->g,((bgra_t*) &src_matrix[i][(j) * 4])->b);
-                /*double auxSumaPorAlpha=(sumaBij+sumaGij+sumaRij)*alpha;
+                double auxSumaPorAlpha=(sumaBij+sumaGij+sumaRij)*alpha;
                 double auxPreDivR=auxSumaPorAlpha*(*p_s).r;
                 double auxPreDivG=auxSumaPorAlpha*(*p_s).g;
                 double auxPreDivB=auxSumaPorAlpha*(*p_s).b;
@@ -54,12 +53,9 @@ void ldr_c    (
                 (*p_d).r=MIN(MAX(ldrR,0),255);
                 (*p_d).g=MIN(MAX(ldrG,0),255);
                 (*p_d).b=MIN(MAX(ldrB,0),255);
-            }*/
-                            printf("%d %d %d %s",((bgra_t*) &src_matrix[i][(j) * 4])->r,((bgra_t*) &src_matrix[i][(j) * 4])->g,((bgra_t*) &src_matrix[i][(j) * 4])->b,"-");
-
+            }
+            
         }
-        printf("\n");
     }
 }
-
 
